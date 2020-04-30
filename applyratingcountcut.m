df2c9b830fec05eb6c50f694dcebba80f771e55d
Tuomo Kalliokoski <tuokall@gmail.com>
2020-04-30 10:02:26 +0300

    skripti ja tuotos.csv jossa minimimääräksi arvioita asetettiin 100 kpl

---
+++ applyratingcountcut.m
@@ -0,0 +1,4 @@
+taulu=readtable('cleanData.csv');     %avataan taulu
+poistettavat = taulu.usersrated < 100; % valitaan ne pelit joilla alle 100 arvioita
+taulu(poistettavat,:) = [];           % poistetaan ne
+writetable(taulu,'morethan100ratings.csv'); % talletetaan uuteen tiedostoon
