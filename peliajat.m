a2e22dd28672d4e631df5e734381cea10ae2b676
Tuomo Kalliokoski <tuokall@gmail.com>
2020-05-08 08:57:44 +0300

    korjattu muuttujan nimi koska ei saa olla sama nimi kuin skriptillä

--- peliajat.m
+++ peliajat.m
@@ -9,5 +9,5 @@ peliaika31to60min = (taulu.playtime>30&taulu.playtime<61);
 peliaika1hto2h = (taulu.playtime>60&taulu.playtime<121);
 peliaikayli2h = taulu.playtime>120;
 
-peliajat = table(name,peliaikaAlle30min,peliaika31to60min,peliaika1hto2h,peliaikayli2h);
-writetable(peliajat,'peliaika.csv');
+peliajattaulu = table(name,peliaikaAlle30min,peliaika31to60min,peliaika1hto2h,peliaikayli2h);
+writetable(peliajattaulu,'peliaika.csv');
